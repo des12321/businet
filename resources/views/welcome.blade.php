38424eed3cb9239b17b3e77a88b655278fdcf266
des12321 <36797161+des12321@users.noreply.github.com>
2018-05-31 11:03:14 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel UPDATE
                 </div>
 
                 <div class="links">
