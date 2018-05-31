d00eddfc0a19a4cdca84f37e19953e3d9e0eb3f2
Johan Juarez <johanjuarezcaruci@gmail.com>
2018-05-31 14:08:51 -0400

    cambio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel UPDATE new
+                    Laravel UPDATE new 200
                 </div>
 
                 <div class="links">
