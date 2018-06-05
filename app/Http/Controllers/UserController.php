117483d80aa3ac34cab6bb7a6dcded376e73202e
Johan Juarez <johanjuarezcaruci@gmail.com>
2018-06-05 09:05:58 -0400

    fillTopBanner

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,41 +43,14 @@ class UserController extends BaseController
         $cookieUser = $request->cookie("businetbybinanceusernamevalueusername");
     
         $url = 'http://18.236.104.133/portal/index.php/welcome/getUserRed/"'+ $cookieUser +'"';
-
+        DD($url);
         $curl = cURL::get($url);
   
        $curlJson = json_decode($curl);
         $data= [
             'data' => $curlJson
         ];
-       /*$res= [
-        'biz_rango' => 'Bitcoiner',
-        'value' => '1000109',
-        'c_doctype_id' => "10000047",
-        'biz_saldo_usd' => '0.00',
-        'biz_saldo_btc' => '0.00',
-        'biz_rangoprox' => 'Bitcoiner Senior',
-        'biz_maxiline' => '4',
-        'biz_soc_der' => '14',
-        'biz_soc_cen' => '6',
-        'biz_soc_izq' => '6',
-        'biz_veder' => '1',
-        'biz_vecent' => '2',
-        'biz_veizq' => '1',
-        'biz_por_der' => '25',
-        'biz_por_cen' => '50',
-        'biz_por_izq' => '25',
-        'biz_por_rango' => '33',
-        'biz_volequipo' => '4'
-       ];
-       //$varss = json_encode($res);
-        //$vars=  $varss->biz_rango;
-      // DD($vars);
-      // $collle = $this->userCollection->manejandoDatos($res);
-      // DD($colle);
-        $data = [
-        'data' => $res
-        ];*/
+    
        return response()->json($data, 200);
     }
 }
